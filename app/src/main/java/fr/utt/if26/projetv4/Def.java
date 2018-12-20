4a0b8c58c7bcdd0be3ce07958afce03d999ca9bc
Mathieu SOL <mathieu.sol@utt.fr>
2018-12-20 16:11:42 +0100

    Created base project files.

---
+++ app/src/main/java/fr/utt/if26/projetv4/Def.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.projetv4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Def extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_def);
+    }
+}
---
+++ app/src/main/java/fr/utt/if26/projetv4/MainActivity.java
@@ -0,0 +1,33 @@
+package fr.utt.if26.projetv4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.listView);
+        String[] values =new String[]{"button1","button2"};
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this,
+                android.R.layout.activity_list_item,android.R.id.text1,values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    Intent myintent= new Intent (view.getContext(),Dico.class);
+                    startActivityForResult(myintent,0);
+                }
+            }
+        });
+    }
+}
