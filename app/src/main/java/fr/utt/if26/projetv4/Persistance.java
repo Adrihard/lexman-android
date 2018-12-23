60a73c6d7286717af664f8c57f3ad8d37d093b56
Adrian Kalmar <adrian.kalmar@protonmail.com>
2018-12-23 15:13:45 +0100

    Implemented method "creerLexique".

--- app/src/main/java/fr/utt/if26/projetv4/Persistance.java
+++ app/src/main/java/fr/utt/if26/projetv4/Persistance.java
@@ -1,12 +1,14 @@
 package fr.utt.if26.projetv4;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.List;
 
-public class Persistance extends SQLiteOpenHelper implements  PersistanceLexiqueInterface, PersistanceTermeInterface{
+public class Persistance extends SQLiteOpenHelper implements PersistanceLexiqueInterface, PersistanceTermeInterface
+{
     private static final String DATABASE_NAME       = "lexiques.db";
     private static final int    DATABASE_VERSION    = 1;
 
@@ -21,48 +23,63 @@ public class Persistance extends SQLiteOpenHelper implements  PersistanceLexique
     private static final String TER_DESCRIPTIF  = "descriptif";
     private static final String TER_LEXIQUE     = "id_lexique";
 
-    private static final String LEXIQUES_CREATE =
+    private static final String LEXIQUES_CREATE
 
-            "CREATE TABLE " + LEX_TABLE + "("
-                    + LEX_ID + " INTEGER, "
-                    + LEX_TITRE + " TEXT NOT NULL, "
-                    + LEX_DESCRIPTIF + " TEXT, "
-                    + "PRIMARY KEY(" + LEX_ID + ") "
-            + ")";
+        = "CREATE TABLE " + LEX_TABLE + "("
+                + LEX_ID + " INTEGER, "
+                + LEX_TITRE + " TEXT NOT NULL, "
+                + LEX_DESCRIPTIF + " TEXT, "
+                + "PRIMARY KEY(" + LEX_ID + ") "
+        + ")";
 
-    private static final String LEXIQUES_DROP =
+    private static final String LEXIQUES_DROP
 
-            "DROP TABLE " + LEX_TABLE;
+        = "DROP TABLE " + LEX_TABLE;
 
-    private static final String TERMES_CREATE =
+    private static final String TERMES_CREATE
 
-            "CREATE TABLE " + TER_TABLE + "("
-                    + TER_ID + " INTEGER, "
-                    + TER_TITRE + " TEXT NOT NULL, "
-                    + TER_DESCRIPTIF + " TEXT, "
-                    + TER_LEXIQUE + " INTEGER, "
-                    + "FOREIGN KEY(" + TER_LEXIQUE + ") "
-                        + "REFERENCES " + LEX_TABLE + "(" + LEX_ID + ") "
-                        + "ON DELETE CASCADE, "
-                    + "PRIMARY KEY(" + TER_ID + ") "
-            + ")";
+        = "CREATE TABLE " + TER_TABLE + "("
+                + TER_ID + " INTEGER, "
+                + TER_TITRE + " TEXT NOT NULL, "
+                + TER_DESCRIPTIF + " TEXT, "
+                + TER_LEXIQUE + " INTEGER, "
+                + "FOREIGN KEY(" + TER_LEXIQUE + ") "
+                    + "REFERENCES " + LEX_TABLE + "(" + LEX_ID + ") "
+                    + "ON DELETE CASCADE, "
+                + "PRIMARY KEY(" + TER_ID + ") "
+        + ")";
 
-    private static final String TERMES_DROP =
+    private static final String TERMES_DROP
 
-            "DROP TABLE " + TER_TABLE;
+        = "DROP TABLE " + TER_TABLE;
+
+    private static String trimAndNullify(String str)
+    {
+        if (str != null)
+        {
+            str = str.trim();
+
+            if (!str.isEmpty())
+                return (str);
+        }
+
+        return (null);
+    }
 
     public Persistance(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase db)
+    {
         db.execSQL(LEXIQUES_CREATE);
         db.execSQL(TERMES_CREATE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
         db.execSQL(TERMES_DROP);
         db.execSQL(LEXIQUES_DROP);
 
@@ -71,8 +88,24 @@ public class Persistance extends SQLiteOpenHelper implements  PersistanceLexique
     }
 
     @Override
-    public boolean creerLexique(String titre, String descriptif) {
-        return false;
+    public boolean creerLexique(String titre, String descriptif)
+    {
+        String _titre       = trimAndNullify(titre);
+        String _descriptif  = trimAndNullify(descriptif);
+
+        ContentValues values = new ContentValues();
+
+        if (_titre != null)
+        {
+            values.put(LEX_TITRE,       _titre);
+            values.put(LEX_DESCRIPTIF,  _descriptif);
+
+            try (SQLiteDatabase db = getWritableDatabase()) {
+                return (db.insert(LEX_TABLE, null, values) != -1);
+            }
+        }
+
+        return (false);
     }
 
     @Override
